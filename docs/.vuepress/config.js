de5affacb24acd3c6ef2bbb41ae67fb14ece7403
EricLeeN1 <179310009@qq.com>
2022-03-10 22:51:51 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  title: "Vue3-Element-Plus-Admin 文档",
+  description: "Vue3-Element-Plus-Admin-Doc 非官方文档开发步骤",
+  theme: "reco",
+  locales: {
+    "/": {
+      lang: "zh-CN",
+    },
+  },
+  themeConfig: {
+    subSidebar: "auto",
+    nav: [
+      { text: "首页", link: "/" },
+      {
+        text: "李祎泽的 JavaScript 博客",
+        items: [
+          { text: "Github", link: "https://github.com/EricLeeN1" },
+          {
+            text: "掘金",
+            link: "https://juejin.cn/user/3280598428825677/posts",
+          },
+        ],
+      },
+    ],
+    sidebar: [
+      {
+        title: "欢迎学习",
+        path: "/",
+        collapsable: false, // 不折叠
+        children: [{ title: "学前必读", path: "/" }],
+      },
+      {
+        title: "基础学习",
+        path: "/base/ConditionalTypes",
+        collapsable: false, // 不折叠
+        children: [
+          { title: "Vue3 Infos", path: "/base/Vue3" },
+          { title: "Element-Plus Infos", path: "/base/Element-Plus" },
+        ],
+      },
+    ],
+  },
+};
---
+++ docs/base/Element-Plus.md
@@ -0,0 +1,5 @@
+---
+title: Element Plus
+author: 李祎泽
+date: "2021-12-12"
+---
